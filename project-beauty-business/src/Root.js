448e880958a9ff8c631c58e885e6f86d5c2e7c14
moonlight1243215 <78422441+moonlight1243215@users.noreply.github.com>
2021-08-05 20:08:55 +0200

    Create react app

---
+++ project-beauty-business/src/Root.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Root = (props) => (
+    <>
+        <h3>Root works</h3>
+    </>
+);
+
+Root.propTypes = {};
+
+export default Root;
