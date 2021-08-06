d266850c29ba9bf2981477d98cf275e83d1c6742
Konrad <KingLoczek@users.noreply.github.com>
2021-08-06 17:15:19 +0200

    Add <nav> to links

--- project-beauty-business/src/components/Menu/Menu.js
+++ project-beauty-business/src/components/Menu/Menu.js
@@ -4,10 +4,12 @@ import './Menu.css';
 
 const Menu = (props) => (
     <div className="background">
+    <nav>
         <a href="#" className="logo">NAZWA</a>
         <a href="#" className="menu-bar">ZAREJESTRUJ SIĘ</a>
         <a href="#" className="menu-bar">ZALOGUJ SIĘ</a>
         <a href="#" className="menu-bar">PROWADZISZ BIZNES BEAUTY</a>
+    </nav>
     </div>
 );
 
